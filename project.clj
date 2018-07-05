0c399ab3427075884efcfed8900653ed951a81e6
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-07-05 16:29:33 +0300

    Bump dependencies, use saml20-clj fork by Kira Systems (supports SHA-256)

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "http://github.com/quephird/saml-test"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [compojure "1.5.2"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [compojure "1.6.1"]
                  [hiccup "1.0.5"]
-                 [org.slf4j/slf4j-api "1.7.22"]
-                 [org.slf4j/slf4j-log4j12 "1.7.22"]
-                 [ring "1.5.1"]
-                 [saml20-clj "0.1.6"]]
+                 [org.slf4j/slf4j-api "1.7.25"]
+                 [org.slf4j/slf4j-log4j12 "1.7.25"]
+                 [ring "1.6.3"]
+                 [kirasystems/saml20-clj "0.1.12"]]
   :plugins [[big-solutions/lein-mvn "0.1.0"]
             [lein-bower "0.5.2"]
             [lein-npm "0.6.2"]]
-  :npm {:dependencies [[saml-idp "0.2.2"]]}
+  :npm {:dependencies [[saml-idp "1.1.0"]]}
   :main saml-test.core)
